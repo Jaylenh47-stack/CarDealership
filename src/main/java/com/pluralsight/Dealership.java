8dfa83d78fb3b7d216ca52ab0e2e7797f5e6b2f9
Jaylenh47-stack <jaylenh47@gmail.com>
2025-10-23 15:39:03 -0400

    initial classes added

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class Main {
+
+    private static void main(String[] args){
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
